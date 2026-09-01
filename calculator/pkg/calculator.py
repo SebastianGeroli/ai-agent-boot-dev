838250814c3d2f72924e381eebe0d10f7446ff9f
SebastianGeroli <sebastiangeroli@gmail.com>
2026-09-01 18:51:44 +0000

    Finished course

--- calculator/pkg/calculator.py
+++ calculator/pkg/calculator.py
@@ -14,8 +14,8 @@ class Calculator:
         self.precedence: dict[str, int] = {
             "+": 1,
             "-": 1,
-            "*": 2,
-            "/": 2,
+            "*": 3,
+            "/": 3,
         }
 
     def evaluate(self, expression: str) -> float | None:
